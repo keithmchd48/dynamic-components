25bd3822ba159ed437892961e863e768dd68a270
keithmchd48 <keithmchd48@gmail.com>
2021-05-08 19:29:50 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <label>Name: </label>
+      <input type="text" v-model="name" />
+    </div>
+
+    <div>
+      <label>Type: </label>
+      <select v-model="vehicleType">
+        <option value="CAR">Car</option>
+        <option value="MOTORCYCLE">Motorcycle</option>
+        <option value="TRUCK">Truck</option>
+      </select>
+    </div>
+
+    <hr />
+
+    <div>
+      Vehicle type: {{ vehicleType }} <br /><br />
+      Vehicle name: {{ name }} <br /><br />
+      Vehicle Configuration: <br />
+      {{ vehicleConfig }}
+    </div>
+
+    <component
+      @on-change-car-config="onChangeCarConfig"
+      @on-change-motorcycle-config="onChangeMotorcycleConfig"
+      @on-change-truck-config="onChangeTruckConfig"
+      :is="selectedVehicleComponent"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Car from "@/components/Car.vue";
+import Motorcycle from "@/components/Motorcycle.vue";
+import Truck from "@/components/Truck.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Car,
+    Motorcycle,
+    Truck,
+  },
+
+  data() {
+    return {
+      name: "",
+      vehicleType: "CAR",
+      vehicleConfig: {},
+    };
+  },
+  computed: {
+    selectedVehicleComponent() {
+      switch (this.vehicleType) {
+        case "CAR":
+          return "Car";
+        case "MOTORCYCLE":
+          return "Motorcycle";
+        case "TRUCK":
+          return "Truck";
+      }
+      return null;
+    },
+  },
+  methods: {
+    onChangeCarConfig(carConfig) {
+      this.vehicleConfig = carConfig;
+    },
+    onChangeMotorcycleConfig(motorcycleConfig) {
+      this.vehicleConfig = motorcycleConfig;
+    },
+    onChangeTruckConfig(truckConfig) {
+      this.vehicleConfig = truckConfig;
+    },
+  },
+};
 </script>
 
 <style>
